d787efbf50057b0acf0ab403852b82a09eac2314
Eric Rodriguez <emorque10@gmail.com>
2025-02-04 17:15:03 -0500

    Styling overhaul

--- projects.js
+++ projects.js
@@ -19,43 +19,44 @@ const homepage = "Home Page";
 // TypeSwitch photo
 const typeswitchPhoto = document.getElementById('typeswitch-photo');
 const typeswitchButton = document.getElementById('typeswitch-button');
-typeswitchButton.textContent = demo;  
+const typeswitchPath = typeswitchButton.querySelector('path');
+
 
 typeswitchButton.addEventListener("click", () => {
-    if (typeswitchButton.textContent === demo) {
+    if (typeswitchPath.getAttribute('d') === "m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393") {
         typeswitchPhoto.src = "gifs/TypeSwitch_demo.gif";
-        typeswitchButton.textContent = homepage;  
+        typeswitchPath.setAttribute('d', "M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5m5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5") 
     } else {
         typeswitchPhoto.src = "img/typeswitch.png";
-        typeswitchButton.textContent = demo;  
+        typeswitchPath.setAttribute('d', "m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393") 
     }
 });
 
 // OT photo
 const otPhoto = document.getElementById('ot-photo');
 const otButton = document.getElementById('ot-button');
-otButton.textContent = demo; 
+const otPath = otButton.querySelector('path');
 
 otButton.addEventListener("click", () => {
-    if (otButton.textContent === demo) {
+    if (otPath.getAttribute('d') === "m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393") {
         otPhoto.src = "gifs/OT_UI_demo.gif";
-        otButton.textContent = homepage;  
+        otPath.setAttribute('d', "M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5m5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5") 
     } else {
         otPhoto.src = "img/octopath.webp";
-        otButton.textContent = demo;  
+        otPath.setAttribute('d', "m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393") 
     }
 });
 
 const steamfiberPhoto = document.getElementById('steamfiber-photo');
 const steamfiberButton = document.getElementById('steamfiber-button');
-steamfiberButton.textContent = demo; 
+const steamfiberPath = steamfiberButton.querySelector('path');
 
 steamfiberButton.addEventListener("click", () => {
-    if (steamfiberButton.textContent === demo) {
+    if (steamfiberPath.getAttribute('d') === "m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393") {
         steamfiberPhoto.src = "gifs/SteamFiber_demo.gif";
-        steamfiberButton.textContent = homepage;  
+        steamfiberPath.setAttribute('d', "M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5m5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5") 
     } else {
         steamfiberPhoto.src = "img/steamfiber.webp";
-        steamfiberButton.textContent = demo;  
+        steamfiberPath.setAttribute('d', "m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393") 
     }
 });
